11cfb8cb20420b4c9f573bda8a8023718e654ade
007AMAN007 <as47986@gmail.com>
2021-07-11 23:27:27 +0530

    changes

--- src/post-script.js
+++ src/post-script.js
@@ -1,4 +1,30 @@
 const postScript = () => {
+    // (function () {
+    //     var script = document.createElement("script");
+    //     script.type = "text/javascript";
+    //     script.src =
+    //         "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"; // use the location of your MathJax
+
+    //     var config =
+    //         "MathJax.Hub.Config({" +
+    //         'extensions: ["tex2jax.js"],' +
+    //         'jax: ["input/TeX","output/HTML-CSS"]' +
+    //         "});" +
+    //         "MathJax.Hub.Startup.onload();";
+
+    //     if (window.opera) {
+    //         script.innerHTML = config;
+    //     } else {
+    //         script.text = config;
+    //     }
+
+    //     script.addEventListener("load", function () {
+    //         MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    //     });
+
+    //     document.getElementsByTagName("head")[0].appendChild(script);
+    // })();
+
     waitForElement(".content-body", 8000)
         .then(function () {
             console.log("content-body element is loaded.. do stuff");
@@ -9,6 +35,7 @@ const postScript = () => {
                 hasInnerContainers: true,
             });
             tocbot.refresh();
+            mathJaxScript();
         })
         .catch(() => {
             console.log("content-body element did not load in 8 seconds");
@@ -40,6 +67,41 @@ function makeAnchorTargetBlank() {
             $(this).attr("target", "_blank");
         }
     });
+}
+
+function mathJaxScript() {
+    // var s = document.createElement("script");
+    // //s.type = "text/javascript";
+    // s.async = true;
+    // s.setAttribute("id", "MathJax-script");
+    // s.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+    // $("head").append(s);
+    // var script = document.createElement("script");
+    // script.type = "text/javascript";
+    // script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"; // use the location of your MathJax
+    // var config =
+    //     "MathJax.Hub.Config({" +
+    //     'extensions: ["tex2jax.js"],' +
+    //     'jax: ["input/TeX","output/HTML-CSS"]' +
+    //     "});" +
+    //     "MathJax.Hub.Startup.onload();";
+    // if (window.opera) {
+    //     script.innerHTML = config;
+    // } else {
+    //     script.text = config;
+    // }
+    // script.addEventListener("load", function () {
+    //     MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    // });
+    // document.getElementsByTagName("head")[0].appendChild(script);
+    window.MathJax = {
+        startup: {
+            ready: () => {
+                MathJax.startup.defaultReady();
+                // MathJax is initialized and you can trigger a typeset.
+            },
+        },
+    };
 }
 
 export default postScript;
