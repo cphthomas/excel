290b1e9980aa64969bf5d27ee95a57ccc678bd48
007AMAN007 <as47986@gmail.com>
2021-09-22 16:18:01 +0530

    .

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -4,7 +4,7 @@ module.exports = {
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `Excel online bog`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Excel online bog`, // This allows an alternative site description for meta data for pages.
+    siteDescriptionMeta: `Excel online bog`, // This allows an alternative site description for meta data for pages
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
